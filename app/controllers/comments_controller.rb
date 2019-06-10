9a62c0f56a95e6f093f198dab1cc5b4469ed5045
mbarrins <maija.barrins@gmail.com>
2019-06-10 15:53:49 +0100

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
+    # byebug
     comment = Comment.create(comment_params)
     redirect_to comment.post
   end
@@ -8,6 +9,10 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    if !!params[:comment][:user_attributes][:username] && params[:comment][:user_attributes][:username] != ""
+      params.require(:comment).permit(:content, :post_id, user_attributes:[:username])
+    else
+      params.require(:comment).permit(:content, :post_id, :user_id)
+    end
   end
 end
